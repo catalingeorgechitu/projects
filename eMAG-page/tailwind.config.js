f4e611cb660b45883f14501757b8c95b4223550a
Catalin Chitu <catalin.george.chitu@gmail.com>
2022-08-15 19:08:25 +0300

    Started to work on the main section of eMAG project, added cards, working on styling a little, need to clean the tailwind code, add more cards, add some reviews to cards and add the footer

--- eMAG-page/tailwind.config.js
+++ eMAG-page/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			fontSize: {
+				micro: '0.5rem',
+			},
+		},
+	},
+	plugins: [],
+};
