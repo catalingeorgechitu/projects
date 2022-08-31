03c0893354a9509d93caa74298f040fc4ddb4be8
Catalin Chitu <catalin.george.chitu@gmail.com>
2022-08-31 17:45:20 +0300

    Finished seo-agency project, need to clean the text, make it responsive, check for bugs, add some missing features

--- shoppoing-cart/src/main.js
+++ shoppoing-cart/src/main.js
@@ -1,109 +1,109 @@
 // Select the buttons on the page
-const selectedProduct = document.getElementsByClassName('product');
-const addQuantity = document.getElementsByClassName('item-add');
-const substractQuantity = document.getElementsByClassName('item-substract');
-const payButton = document.getElementsByClassName('pay');
+const selectedProduct = document.getElementsByClassName("product");
+const addQuantity = document.getElementsByClassName("item-add");
+const substractQuantity = document.getElementsByClassName("item-substract");
+const payButton = document.getElementsByClassName("pay");
 let totalPrice = 0;
 
 // Add the selected product to cart
 for (let i = 0; i < selectedProduct.length; i++) {
-	let product = selectedProduct[i];
-	product.addEventListener('click', function () {
-		let title = product.getElementsByClassName('product-title')[0].innerText;
-		let price = parseFloat(
-			product
-				.getElementsByClassName('product-price')[0]
-				.innerText.replace(' Lei', '')
-		).toFixed(2);
+  let product = selectedProduct[i];
+  product.addEventListener("click", function () {
+    let title = product.getElementsByClassName("product-title")[0].innerText;
+    let price = parseFloat(
+      product
+        .getElementsByClassName("product-price")[0]
+        .innerText.replace(" Lei", "")
+    ).toFixed(2);
 
-		addToCart(title, price);
-		updateCartPrice();
-	});
+    addToCart(title, price);
+    updateCartPrice();
+  });
 }
 
 // Add click event to increase the quantity of the product in cart
 for (let i = 0; i < addQuantity.length; i++) {
-	let button = addQuantity[i];
-	button.addEventListener('click', addQuantityFunction);
+  let button = addQuantity[i];
+  button.addEventListener("click", addQuantityFunction);
 }
 
 // Add click event to decrease the quantity of the product in cart
 for (let i = 0; i < substractQuantity.length; i++) {
-	let button = substractQuantity[i];
-	button.addEventListener('click', substractQuantityFunction);
+  let button = substractQuantity[i];
+  button.addEventListener("click", substractQuantityFunction);
 }
 
 // Decrease the quantity of the product in cart function
 function substractQuantityFunction(e) {
-	let target = e.currentTarget.parentNode;
-	let targetQuantity = parseInt(
-		target.getElementsByClassName('cart-quantity')[0].innerText
-	);
-	if (targetQuantity > 1) {
-		target.getElementsByClassName('cart-quantity')[0].innerText =
-			targetQuantity - 1;
-	} else {
-		target.getElementsByClassName('cart-quantity')[0].innerText = 1;
-	}
-	updateCartPrice();
+  let target = e.currentTarget.parentNode;
+  let targetQuantity = parseInt(
+    target.getElementsByClassName("cart-quantity")[0].innerText
+  );
+  if (targetQuantity > 1) {
+    target.getElementsByClassName("cart-quantity")[0].innerText =
+      targetQuantity - 1;
+  } else {
+    target.getElementsByClassName("cart-quantity")[0].innerText = 1;
+  }
+  updateCartPrice();
 }
 
 // Increase the quantity of the product in cart function
 function addQuantityFunction(e) {
-	let target = e.currentTarget.parentNode;
-	let targetQuantity = parseInt(
-		target.getElementsByClassName('cart-quantity')[0].innerText
-	);
+  let target = e.currentTarget.parentNode;
+  let targetQuantity = parseInt(
+    target.getElementsByClassName("cart-quantity")[0].innerText
+  );
 
-	if (targetQuantity >= 1) {
-		target.getElementsByClassName('cart-quantity')[0].innerText =
-			targetQuantity + 1;
-	} else {
-		target.getElementsByClassName('cart-quantity')[0].innerText = 1;
-	}
-	updateCartPrice();
+  if (targetQuantity >= 1) {
+    target.getElementsByClassName("cart-quantity")[0].innerText =
+      targetQuantity + 1;
+  } else {
+    target.getElementsByClassName("cart-quantity")[0].innerText = 1;
+  }
+  updateCartPrice();
 }
 
 // Updates row prices and total price of the cart function
 function updateCartPrice() {
-	let cartItemContainer = document.getElementsByClassName('cart-container')[0];
-	let cartRows = cartItemContainer.getElementsByClassName('cart-row');
-	let total = 0;
+  let cartItemContainer = document.getElementsByClassName("cart-container")[0];
+  let cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  let total = 0;
 
-	for (let i = 0; i < cartRows.length; i++) {
-		let cartRow = cartRows[i];
-		let quantityElement = parseInt(
-			cartRow.getElementsByClassName('cart-quantity')[0].innerText
-		);
-		let itemPriceElement = parseFloat(
-			cartRow.getElementsByClassName('item-price')[0].innerText
-		);
-		price = itemPriceElement * quantityElement;
+  for (let i = 0; i < cartRows.length; i++) {
+    let cartRow = cartRows[i];
+    let quantityElement = parseInt(
+      cartRow.getElementsByClassName("cart-quantity")[0].innerText
+    );
+    let itemPriceElement = parseFloat(
+      cartRow.getElementsByClassName("item-price")[0].innerText
+    );
+    price = itemPriceElement * quantityElement;
 
-		document.getElementsByClassName('cart-price')[i].innerText =
-			price.toFixed(2);
-		total += price;
-	}
+    document.getElementsByClassName("cart-price")[i].innerText =
+      price.toFixed(2);
+    total += price;
+  }
 
-	document.getElementsByClassName(
-		'total-price'
-	)[0].innerText = `RON ${total.toFixed(2)}`;
+  document.getElementsByClassName(
+    "total-price"
+  )[0].innerText = `RON ${total.toFixed(2)}`;
 }
 
 // Adds the selected item to cart and creates the structure for the cart row
 function addToCart(title, price) {
-	let cartRow = document.createElement('div');
-	cartRow.classList.add('cart-row', 'mx-6', 'my-2', 'flex');
-	let container = document.getElementsByClassName('cart-container')[0];
-	let cartItemNames = container.getElementsByClassName('cart-title');
-	for (let i = 0; i < cartItemNames.length; i++) {
-		if (cartItemNames[i].innerText == title) {
-			alert('Produsul a fost deja adaugat in cos');
-			return;
-		}
-	}
+  let cartRow = document.createElement("div");
+  cartRow.classList.add("cart-row", "mx-6", "my-2", "flex");
+  let container = document.getElementsByClassName("cart-container")[0];
+  let cartItemNames = container.getElementsByClassName("cart-title");
+  for (let i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      alert("Produsul a fost deja adaugat in cos");
+      return;
+    }
+  }
 
-	let cartRowContents = `
+  let cartRowContents = `
 		<div class="cart-title w-7/12">${title}</div>
 		<div
 		class="item-substract mr-2 flex h-8 w-8 items-center justify-center rounded-md border border-slate-800 transition duration-100 hover:cursor-pointer hover:bg-slate-900 hover:text-white"
@@ -121,29 +121,29 @@ function addToCart(title, price) {
 		<div class="cart-price ml-12 w-2/12">0</div>
 
 	`;
-	cartRow.innerHTML = cartRowContents;
-	container.append(cartRow);
-	cartRow
-		.getElementsByClassName('item-add')[0]
-		.addEventListener('click', addQuantityFunction);
-	cartRow
-		.getElementsByClassName('item-substract')[0]
-		.addEventListener('click', substractQuantityFunction);
-	updateCartPrice();
+  cartRow.innerHTML = cartRowContents;
+  container.append(cartRow);
+  cartRow
+    .getElementsByClassName("item-add")[0]
+    .addEventListener("click", addQuantityFunction);
+  cartRow
+    .getElementsByClassName("item-substract")[0]
+    .addEventListener("click", substractQuantityFunction);
+  updateCartPrice();
 }
 
 // Stores the total cart price onto the local storage
 function payNow() {
-	total = parseFloat(
-		document
-			.getElementsByClassName('total-price')[0]
-			.innerText.replace('RON ', '')
-	);
-	localStorage.setItem('cartPrice', total);
-	if (total > 0) {
-		window.location.replace(window.location.href.replace('index', '/src/payment'));
-	} else {
-		alert('Cosul tau de cumparaturi este gol');
-	}
+  total = parseFloat(
+    document
+      .getElementsByClassName("total-price")[0]
+      .innerText.replace("RON ", "")
+  );
+  localStorage.setItem("cartPrice", total);
+  if (total > 0) {
+    console.log(window.location.href);
+    window.location.replace(window.location.href + "src/payment.html");
+  } else {
+    alert("Cosul tau de cumparaturi este gol");
+  }
 }
-
--- shoppoing-cart/src/payment.js
+++ shoppoing-cart/src/payment.js
@@ -2,72 +2,72 @@
 
 // Gets total cart price
 window.onload = function () {
-	getTotalCartPrice();
+  getTotalCartPrice();
 };
 
-const selectedMoney = document.getElementsByClassName('money');
-const totalOutput = document.getElementsByClassName('total-output')[0];
-const paidOutput = document.getElementsByClassName('paid-output')[0];
-const restOutput = document.getElementsByClassName('rest-output')[0];
-const receiveOutput = document.getElementsByClassName('receive-output')[0];
+const selectedMoney = document.getElementsByClassName("money");
+const totalOutput = document.getElementsByClassName("total-output")[0];
+const paidOutput = document.getElementsByClassName("paid-output")[0];
+const restOutput = document.getElementsByClassName("rest-output")[0];
+const receiveOutput = document.getElementsByClassName("receive-output")[0];
 
 // Back button function
 function backButton() {
-	window.location.replace(
-		window.location.href.replace('/src/payment', 'index')
-	);
+  window.location.replace(
+    window.location.href.replace("/src/payment.html", "")
+  );
 }
 
 // Loops through the selected money value
 for (let i = 0; i < selectedMoney.length; i++) {
-	let money = selectedMoney[i];
-	money.addEventListener('click', function () {
-		moneyValue = parseFloat(money.getElementsByClassName('value')[0].innerText);
-		moneyPaid(moneyValue);
+  let money = selectedMoney[i];
+  money.addEventListener("click", function () {
+    moneyValue = parseFloat(money.getElementsByClassName("value")[0].innerText);
+    moneyPaid(moneyValue);
 
-		restToBePaid();
-		restToReceive();
-	});
+    restToBePaid();
+    restToReceive();
+  });
 }
 
 // Calculates the total money paid until now and verifies if the paid value is bigger than the total value
 function moneyPaid(moneyValue) {
-	let paid = parseFloat(paidOutput.innerText);
+  let paid = parseFloat(paidOutput.innerText);
 
-	if (paid > parseFloat(totalOutput.innerText)) {
-		alert('Valoarea platita depaseste totalul de plata');
-	} else {
-		paid += moneyValue;
-	}
+  if (paid > parseFloat(totalOutput.innerText)) {
+    alert("Valoarea platita depaseste totalul de plata");
+  } else {
+    paid += moneyValue;
+  }
 
-	paidOutput.innerText = paid.toFixed(2);
+  paidOutput.innerText = paid.toFixed(2);
 }
 
 // Calculates the rest to be paid by the client
 function restToBePaid() {
-	rest = parseFloat(totalOutput.innerText) - paidOutput.innerText;
+  rest = parseFloat(totalOutput.innerText) - paidOutput.innerText;
 
-	if (rest < 0) {
-		restOutput.innerText = (0.0).toFixed(2);
-	} else {
-		restOutput.innerText = rest.toFixed(2);
-	}
+  if (rest < 0) {
+    restOutput.innerText = (0.0).toFixed(2);
+  } else {
+    restOutput.innerText = rest.toFixed(2);
+  }
 }
 
 // Calculates the rest to be received by the client
 function restToReceive() {
-	receive = paidOutput.innerText - parseFloat(totalOutput.innerText);
+  receive = paidOutput.innerText - parseFloat(totalOutput.innerText);
 
-	if (receive < 0) {
-		receiveOutput.innerText = (0.0).toFixed(2);
-	} else {
-		receiveOutput.innerText = receive.toFixed(2);
-	}
+  if (receive < 0) {
+    receiveOutput.innerText = (0.0).toFixed(2);
+  } else {
+    receiveOutput.innerText = receive.toFixed(2);
+  }
 }
 
 // Gets the total cart price from the local storage
 function getTotalCartPrice() {
-	totalOutput.innerText = parseFloat(localStorage.getItem('cartPrice')).toFixed(
-		2
-	);
+  totalOutput.innerText = parseFloat(localStorage.getItem("cartPrice")).toFixed(
+    2
+  );
 }
